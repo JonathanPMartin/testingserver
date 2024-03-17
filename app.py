7f71fd57b51b0c9586c50c5c47cfc1c0745af5f0
Jonathan <jonathan_P_Martin@outlook.com>
2024-03-17 15:04:38 +0000

    making small change hoping it fixes the problem

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Deployed!</h1><style>body { display: flex; align-items: center; justify-content: center; height: 100vh; }</style>'
+if __name__ == "__main__":
+    # Run the Flask app on the public IP address of your server
+    app.run(host='130.162.188.246', port=8000)
